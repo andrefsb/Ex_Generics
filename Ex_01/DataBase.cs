bb7b526193245ee4ebc7a05f76293d77f6969f13
André Felipe Sampaio Boschetti <andresampaio@id.uff.br>
2022-07-12 21:52:30 -0300

    DataBase creation

---
+++ Ex_01/DataBase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Ex_01
+{
+    internal class DataBase
+    {
+        static string dbPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "dictionary.xml");
+        static List<Terms> dictionary;
+        static DataBase()
+        {
+            if (!File.Exists(dbPath))
+            {
+                var dictionary0 = new List<Terms>();
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Terms>));
+                TextWriter write0 = new StreamWriter(dbPath);
+
+                serializer.Serialize(write0, dictionary0);
+
+                write0.Close();
+            }
+            dictionary = LoadDb();
+
+        }
+
+        internal static int GetEmployeesCount()
+        {
+            return dictionary.Count;
+        }
+
+        private static List<Terms> LoadDb()
+        {
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Terms>));
+                TextReader reader = new StreamReader(dbPath);
+                var objeto = serializer.Deserialize(reader);
+
+                reader.Close();
+                return (List<Terms>)objeto;
+            }
+        }
+
+        public static void SaveDb(string term, string description, List<Terms> dictionary)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Terms>));
+            TextWriter write = new StreamWriter(dbPath);
+            DataBase.dictionary.Add(new Terms(term, description));
+            serializer.Serialize(write, term);
+            write.Close();
+        }
+
+        //public static Terms GetDescriptionByTerm(string logginEntry)
+        //{
+        //    foreach (var t in dictionary)
+        //    {
+        //        if (Entry == term.)
+        //            return term;
+        //    }
+        //    return null;
+        //}
+        public static List<Terms> GetAllTerms()
+        {
+            return dictionary.AsReadOnly().ToList();
+        }
+    }
+}
+
---
+++ Ex_01/Terms.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_01
+{
+    internal class Terms
+    {
+        public string Term { get; set; }
+        public string Description { get; set; }
+
+        public Terms(string term, string description)
+        {
+            Term = term;
+            Description = description;
+        }
+    }
+}
